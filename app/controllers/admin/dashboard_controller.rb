74498062894f8adb20d7fe5a7b1bb4dc395b5761
Peteami <peteami@hotmail.com>
2020-10-15 17:43:20 +0000

    Admin dashboard count added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  # before_action :admin_authenticate
+  # http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'], password: ENV['HTTP_BASIC_PASSWORD']
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
